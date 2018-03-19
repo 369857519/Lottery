250d8f8e6d9cb421f261c0553ba378e6d00009e3
qilianshan@git.n.xiaomi.com <qilianshan@git.n.xiaomi.com>
2018-03-19 22:46:53 +0800

    lottery
    Signed-off-by: qilianshan@git.n.xiaomi.com <qilianshan@git.n.xiaomi.com>

---
+++ contracts/Lottery.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.17;
+
+contract Lottery{
+  //门票
+  uint public ticketPriace;
+
+  //开始时间
+  uint public startTime;
+
+  //下注时间和等待时间
+  uint public gameTime;
+  uint public watingTime;
+
+  //
+  function Lottery(){
+
+  }
+  function Draw(){
+
+  }
+}
